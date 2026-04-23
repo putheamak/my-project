898c011abd79cd248c1524b2bb8d68a6a10d3fc0
Puthea Mak <puthea.mak@vital.com.kh>
2026-04-23 08:40:18 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 👇 IMPORTANT: replace "my-project" with your GitHub repo name
-  base: '/my-project/',
+  base: '/my-project/',   // matches your repo name
 })
